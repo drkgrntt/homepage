d7d8d6f5b98c4cb9fad64d68a4ca31191f907984
Derek Garnett <drkgrntt@gmail.com>
2021-08-29 19:53:50 -0500

    add clock

---
+++ src/components/Clock.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2 class="time">{{ time }}</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Clock",
+  data() {
+    return {
+      interval: 0,
+      time: new Date().toLocaleTimeString()
+    }
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.time = new Date().toLocaleTimeString()
+    }, 1000)
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  }
+});
+</script>
+
+<style scoped lang="scss">
+  .time {
+    font-size: 3rem;
+  }
+</style>
